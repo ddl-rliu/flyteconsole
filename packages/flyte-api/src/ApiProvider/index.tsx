c18d3d3a5ce0dd819dce7721c21af61649a2413d
ddl-rliu <richard.liu@dominodatalab.com>
2024-05-06 13:25:52 -0700

    x

--- packages/flyte-api/src/ApiProvider/index.tsx
+++ packages/flyte-api/src/ApiProvider/index.tsx
@@ -37,7 +37,7 @@ export const FlyteApiProvider = (props: FlyteApiProviderProps) => {
 
   // Whenever we detect expired credentials, trigger a login redirect automatically
   React.useEffect(() => {
-    if (oginExpired) {
+    if (loginExpired) {
       window.location.href = getLoginUrl(flyteApiDomain);
     }
   }, [loginExpired]);
